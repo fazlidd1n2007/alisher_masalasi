1a232a24e51f4f15e43125a21ffb47e059e6db66
Fazlidd1n2007 <muhammadiyevfazliddin15@gmail.com>
2023-11-10 20:08:45 +0500

    alish tugadi

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//ALISH HAQIDA MASALA
+
+var elHeading = document.querySelector(".heading")
+
+var jamisumma = 8317488.6
+
+
+if (jamisumma<=83000000){
+    elHeading.textContent = ("Oq yo'l, Alisher!");
+}
+
+ else if (jamisumma) {
+    elHeading.textContent = ("Alisher, ozgina sabr qilish kerak bo'larkan");
+ }
